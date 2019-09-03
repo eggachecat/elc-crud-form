9ef1e9a28868e3ea5fb858ec01e86fec61402d8d
eggahecat <sunao_0626@hotmail.com>
2019-09-03 18:31:37 +0800

    update

--- bin/elc-crud-form.js
+++ bin/elc-crud-form.js
@@ -6,15 +6,17 @@ import fs from 'fs'
 const global_dict = {
     __ELC_CRUD__TAG_DICT: {},
     __ELC_CRUD__BADGE_DICT: {},
+    __ELC_CRUD__MOCK_DATA: [],
     __ELC_CRUD__MOCK_DATA_ROW: {},
-    __ELC_CRUD__NAME: `User`, // 模型的名称: 首字母大写
-    __ELC_CRUD__MODEL: `user`, // 模型的model的名称
+    __ELC_CRUD__NAME: `AccountManagement`, // 模型的名称: 首字母大写
+    __ELC_CRUD__MODEL: `accountmanagement`, // 模型的model的名称
+    __ELC_CRUD__API_NAME: `account-management`,
     __ELC_CRUD__COLUMNS: null,
     __ELC_CRUD__CREATE_OR_UPDATE_FORM_ITEMS: null,// items的字符串
     __ELC_CRUD__BUILDING_MOCK_DATA: null, // mock的数据
-    __ELC_CRUD__CLASS_NAME: `User`, // export的class的名称
-    __ELC_CRUD__BUILDING_RESPONSIVE: `@BuildingResponsive`, // 是否建筑响应?
-    __ELC_CRUD__VARIABLE_FORM: `CreateOrUpdateUserFormRef`, // CU的modal的名称
+    __ELC_CRUD__CLASS_NAME: `AccountManagement`, // export的class的名称
+    __ELC_CRUD__BUILDING_RESPONSIVE: `@ELCBuildingResponsive`, // 是否建筑响应?
+    __ELC_CRUD__VARIABLE_FORM: `CreateOrUpdateAccountForm`, // CU的modal的名称
     __ELC_CRUD__SEARCH_FORM: null // 搜索的字段
 }
 
@@ -37,7 +39,7 @@ function createBadgeColumn({ prefix, dataIndex, enumerates, multiple = false }) 
     }
     global_dict.__ELC_CRUD__BADGE_DICT[dataIndex] = enumerates.reduce((dict, item, index) => {
         dict[item] = {
-            msgId: `formatMessage({ id: '${prefix}.${dataIndex}.${item}' })`,
+            msgId: `${prefix}.${dataIndex}.${item}`,
             color: ELC_COLORS[index]
         }
         return dict
@@ -86,7 +88,7 @@ function createTagColumn({ prefix, dataIndex, enumerates, multiple = false }) {
 
     global_dict.__ELC_CRUD__TAG_DICT[dataIndex] = enumerates.reduce((dict, item, index) => {
         dict[item] = {
-            msgId: `formatMessage({ id: '${prefix}.${dataIndex}.${item}' })`,
+            msgId: `${prefix}.${dataIndex}.${item}`,
             color: ELC_COLORS[index]
         }
         return dict
@@ -143,9 +145,9 @@ function createFormItemSelect({ prefix, dataIndex, dictName, required = false })
                 rules: [{ required: ${required} }],
             })(
                 <Select>
-                    {Object.keys(${dictName}[${dataIndex}]).map(k => (
-                        <Option key={${dictName}[${dataIndex}][k].msgId} value={k}>
-                            {formatMessage({ id: ${dictName}[${dataIndex}][k].msgId })}
+                    {Object.keys(${dictName}['${dataIndex}']).map(k => (
+                        <Option key={${dictName}['${dataIndex}'][k].msgId} value={k}>
+                            {formatMessage({ id: ${dictName}['${dataIndex}'][k].msgId })}
                         </Option>
                     ))}
                 </Select>
@@ -170,8 +172,9 @@ function generateSearchForm(row) {
     if (row.searchable) {
         switch (row.type) {
             case 'tag':
+                return createFormItemSelect({ ...row, dictName: 'TAG_DICT' })
             case 'badge':
-                return createFormItemSelect(row)
+                return createFormItemSelect({ ...row, dictName: 'BADGE_DICT' })
             default:
                 return createFormItemText(row)
         }
@@ -182,8 +185,9 @@ function generateSearchForm(row) {
 function generateCreateOrUpdateForm(row) {
     switch (row.type) {
         case 'tag':
+            return createFormItemSelect({ ...row, dictName: 'TAG_DICT' })
         case 'badge':
-            return createFormItemSelect(row)
+            return createFormItemSelect({ ...row, dictName: 'BADGE_DICT' })
         default:
             return createFormItemText(row)
     }
@@ -196,9 +200,8 @@ function fillTemplate(template) {
 }
 
 function test() {
-    const prefix = 'user'
     const configs = [
-        { dataIndex: 'username', type: 'text', searchable: false, multiple: true },
+        { dataIndex: 'username', type: 'text', searchable: true, multiple: true },
         { dataIndex: 'roles', type: 'tag', enumerates: ['0', '1'], searchable: true, multiple: true },
         { dataIndex: 'auth', type: 'badge', enumerates: ['series', 'normal'], searchable: true, multiple: true },
     ]
@@ -206,13 +209,26 @@ function test() {
     // console.log();
     // console.log(configs.map(config => generateCreateOrUpdateForm({ prefix, ...config })).join(""));
 
-    global_dict.__ELC_CRUD__COLUMNS = `{${configs.map(config => generateColumn({ prefix, ...config })).join("")}}`
-    global_dict.__ELC_CRUD__SEARCH_FORM = configs.map(config => generateSearchForm({ prefix, ...config })).join("")
-    global_dict.__ELC_CRUD__CREATE_OR_UPDATE_FORM_ITEMS = configs.map(config => generateCreateOrUpdateForm({ prefix, ...config })).join("")
+    global_dict.__ELC_CRUD__COLUMNS = configs.map(config => generateColumn({ prefix: global_dict.__ELC_CRUD__NAME, ...config })).join("")
+    global_dict.__ELC_CRUD__SEARCH_FORM = configs.map(config => ` 
+        <Col md={6} sm={24}>
+            ${generateSearchForm({ prefix: global_dict.__ELC_CRUD__NAME, ...config })}
+        </Col>
+    `).join("")
+    global_dict.__ELC_CRUD__CREATE_OR_UPDATE_FORM_ITEMS = configs.map(config => generateCreateOrUpdateForm({ prefix: global_dict.__ELC_CRUD__NAME, ...config })).join("")
 
     console.log(global_dict.__ELC_CRUD__MOCK_DATA_ROW)
-    let template = fillTemplate(fs.readFileSync(__dirname + '/template_create_crud_form.txt').toString());
-    console.log(template)
+    const pageTemplate = fillTemplate(fs.readFileSync(__dirname + '/template_create_crud_form.txt').toString());
+    console.log(pageTemplate)
+    fs.writeFileSync('D:\\Workspace\\elc\\v2.preview.pro.ant.design\\src\\pages\\Account\\Settings\\AccountManagement.js', pageTemplate)
+
+
+    const modelTemplate = fillTemplate(fs.readFileSync(__dirname + '/template_create_crud_form_model.txt').toString());
+    fs.writeFileSync('D:\\Workspace\\elc\\v2.preview.pro.ant.design\\src\\pages\\Account\\Settings\\models\\accountmanagement.js', modelTemplate)
+
+    global_dict.__ELC_CRUD__MOCK_DATA = [global_dict.__ELC_CRUD__MOCK_DATA_ROW]
+    const apiTemplate = fillTemplate(fs.readFileSync(__dirname + '/template_create_crud_form_api.txt').toString());
+    fs.writeFileSync('D:\\Workspace\\elc\\v2.preview.pro.ant.design\\src\\services\\api-account-management.js', apiTemplate)
 
 }
 
